b59a31ad1333a6d90c5032d5467205c0560d2e7e
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-09-15 08:27:06 -0400

    five

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -18,7 +18,7 @@ export default function Form(props) {
       return (
         <form className='form-container' onSubmit={onSubmit}>
           <div className='form-group inputs'>
-            <label>Team Member
+            <label>Teamster
               <input
               // type="color"
                 type="text"
